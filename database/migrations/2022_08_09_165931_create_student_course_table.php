ddc4809528edff2d2b50883aa60411c656096321
foxstudent102655 <andreypalamarchuk1996@gmail.com>
2022-08-09 20:17:41 +0300

    added models and migrations

--- database/migrations/2022_08_09_165931_create_student_course_table.php
+++ database/migrations/2022_08_09_165931_create_student_course_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('student_course', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
-            $table->timestamps();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('course_id')->constrained();
         });
     }
 
@@ -32,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('student_courses');
     }
 };
